3fff5ed1fe610aaa4693342c11411e3446f9465c
Tolavio Silva <tolavio.silva@inacom.gov.ao>
2021-04-29 10:51:52 +0100

    Preparar o ambiente para listar os produtos

--- cliente/src/app/app.module.ts
+++ cliente/src/app/app.module.ts
@@ -5,11 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HeaderComponent } from './componentes/header/header.component';
+import { ProdutoListComponent } from './componentes/protudos/produto-list/produto-list.component';
+import { ProdutoCreateComponent } from './componentes/protudos/produto-create/produto-create.component';
+import { ProdutoShowComponent } from './componentes/protudos/produto-show/produto-show.component';
+import { ProdutoDeleteComponent } from './componentes/protudos/produto-delete/produto-delete.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ProdutoListComponent,
+    ProdutoCreateComponent,
+    ProdutoShowComponent,
+    ProdutoDeleteComponent
   ],
   imports: [
     BrowserModule,
---
+++ cliente/src/app/componentes/protudos/produto-list/produto-list.component.ts
@@ -0,0 +1,21 @@
+import { ProdutoService } from '../../../core/service/produco.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-produto-list',
+  templateUrl: './produto-list.component.html',
+  styleUrls: ['./produto-list.component.css']
+})
+export class ProdutoListComponent implements OnInit {
+
+  public prods = [];
+
+  constructor(private prodService:ProdutoService) { }
+
+  ngOnInit(): void {
+    this.prodService
+    .index()
+    .subscribe((arg:any) => {this.prods = arg.data})
+  }
+
+}
